b756831ebb3d625455da259427f409510fc8dfe5
Saurabh Vats <saurabh.vats@irissoftware.com>
2018-04-04 12:59:12 +0530

    first commit

---
+++ DummyWebApi/Controller/EmployeeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace DummyWebApi.Controller
+{
+    public class EmployeeController:ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            return Ok(new { FirstName = "Saurabh", LastName = "Vats" });
+        }
+    }
+}
---
+++ DummyWebApi/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Web.Http;
+using Topshelf;
+
+namespace DummyWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = ConfigureApi();
+            app.UseWebApi(config);
+        }
+
+        private HttpConfiguration ConfigureApi()
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+            return config;
+        }
+    }
+    public class DummyService
+    {
+        private IDisposable _app;
+
+        public void Start()
+        {
+            _app = WebApp.Start<Startup>("http://localhost:8085");
+        }
+
+        public void Stop()
+        {
+            if (_app != null)
+                _app.Dispose();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>                                   //1
+            {
+                x.Service<DummyService>(s =>                                   //2
+                {
+                    s.ConstructUsing(name => new DummyService());                //3
+                    s.WhenStarted(tc => tc.Start());                         //4
+                    s.WhenStopped(tc => tc.Stop());                          //5
+                });
+                x.RunAsLocalSystem();                                       //6
+
+                x.SetDescription("Sample Topshelf Host");                   //7
+                x.SetDisplayName("Stuff");                                  //8
+                x.SetServiceName("Stuff");                                  //9
+            });                                                             //10
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
